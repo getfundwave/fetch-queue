955a3531d7c1f43f1381a7886e96f357811cc040
hemantlakhotia204 <hemantlakhotia204@gmail.com>
2023-09-13 16:17:52 +0530

    created index.js, updated readme, added action

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # fetch-queue
-Queue for fetch requests
+
+@fundwave/fetch-queue is a client-side library that allows you to queue your app's fetch requests 
+
+## Installation Instructions
+
+```sh
+npm install @fundwave/fetch-queue
+```
+
+### Initialization
+
+```js
+import { FetchQueue as ApiFetchQueue } from "@fundwave/fetch-queue";
+
+// value of concurrent defaults to 3
+// concurrent defines the number of fetch requests to be send to server at a time
+const FetchQueue = ApiFetchQueue({ concurrent: 2 });
+FetchQueue.initQueue();
+
+// Note: Works with node.js fetch for now.
+```
---
+++ src/index.js
@@ -0,0 +1,50 @@
+export class FetchQueue {
+  constructor(options) {
+    this.concurrent = options?.concurrent || 3;
+    this.priority = options?.priority || {};
+  }
+
+  initQueue() {
+    globalThis.fetch = this._f_fetch;
+  }
+
+  _f_fetch = (() => {
+    const fetch = window.fetch;
+    let activeRequests = 0;
+    const queue = [];
+    const urlInQueue = [];
+
+    const run = async (url, options) => {
+      activeRequests++;
+      console.log(activeRequests, Date.now(), this.concurrent);
+      try {
+        const response = await fetch(url, options);
+        return response;
+      } finally {
+        activeRequests--;
+        if (queue.length > 0) {
+          const nextTask = queue.shift();
+          urlInQueue.shift();
+          localStorage.setItem("queue", urlInQueue);
+          nextTask();
+        }
+      }
+    };
+
+    return (url, options) => {
+      const task = () => run(url, options);
+
+      if (activeRequests < this.concurrent) {
+        return task();
+      } else {
+        return new Promise((resolve, reject) => {
+          queue.push(() => {
+            task().then(resolve).catch(reject);
+          });
+          urlInQueue.push(url.split("/").slice(-1));
+          localStorage.setItem("queue", urlInQueue);
+        });
+      }
+    };
+  })();
+}
