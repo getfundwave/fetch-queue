9d367e158c3046f2deb755aab9f005237f6c4ace
hemantlakhotia204 <hemantlakhotia204@gmail.com>
2023-09-15 16:04:13 +0530

    test cases added + js docs added

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,83 @@
 import { FetchQ, FetchQueueConfig } from "./interfaces/index.js";
 
+/**
+ * The `FetchQueue` class is a utility class that allows for managing and controlling concurrent fetch requests.
+ * It ensures that the number of active requests does not exceed a specified limit, and queues any additional requests until a slot becomes available.
+ */
 export class FetchQueue {
-  concurrent: number;
-  activeRequests: number;
-  queue: Array<() => void>;
+  /**
+   * The maximum number of concurrent fetch requests allowed.
+   */
+  private concurrent: number;
+  /**
+   * The current number of active fetch requests.
+   */
+  private activeRequests: number;
+  /**
+   * A queue of tasks to be executed when a slot becomes available for a new fetch request.
+   */
+  private queue: Array<() => void>;
 
-  constructor(options: FetchQueueConfig) {
+  /**
+   * Initializes a new instance of the FetchQueue class with an optional FetchQueueConfig object.
+   * If no options are provided, the default concurrent value is set to 3.
+   * @param options - The FetchQueueConfig object containing the concurrent value.
+   */
+  constructor(options?: FetchQueueConfig) {
     this.concurrent = options?.concurrent || 3;
     this.activeRequests = 0;
     this.queue = [];
+    if (typeof this.concurrent !== "number")
+      throw Error("Concurrent should be a number.");
+    if (this.concurrent < 0)
+      throw Error("Concurrent should be greater than zero.");
   }
 
-  run = async (
+  /**
+   * Executes a fetch request with the specified URL, fetch function, and options.
+   * Increments the activeRequests count and handles queuing of additional requests if the maximum concurrent limit is reached.
+   * @param url - The URL for the fetch request.
+   * @param fetch - The fetch function to use for the request.
+   * @param options - The options for the fetch request.
+   * @returns A Promise that resolves to the fetch response.
+   */
+  private run = async (
     url: RequestInfo | URL,
     fetch: FetchQ,
     options?: RequestInit
   ): Promise<Response> => {
     this.activeRequests++;
-    console.log(this.activeRequests, Date.now(), this.concurrent);
     try {
       const response: Response = await fetch(url, options);
       return response;
     } finally {
       this.activeRequests--;
-      if (this.queue.length > 0) {
-        const nextTask = this.queue.shift();
-        nextTask!();
-      }
+      this.emitRequestCompletedEvent();
     }
   };
 
+  /**
+   * Executes the next task in the queue when a fetch request is completed.
+   */
+  private emitRequestCompletedEvent(): void {
+    if (this.queue.length > 0) {
+      const nextTask = this.queue.shift();
+      nextTask!();
+    }
+  }
+
+  /**
+   * Initializes the global fetch function to use the FetchQueue's internal fetch implementation.
+   */
   initQueue() {
     global.fetch = this.f_fetch;
   }
 
+  /**
+   * The internal fetch implementation that handles queuing of fetch requests.
+   */
   private f_fetch = (() => {
-    const fetch = window.fetch;
+    const fetch = global.fetch;
 
     return (
       url: RequestInfo | URL,
---
+++ tests/index.test.ts
@@ -0,0 +1,75 @@
+import { FetchQueue } from "../src/index.ts";
+
+describe("FetchQueue", () => {
+  // Initializes FetchQueue with negative concurrent value
+  it("should not initialize FetchQueue with negative concurrent value", () => {
+    expect(() => {
+      new FetchQueue({ concurrent: -1 });
+    }).toThrow();
+  });
+
+  // Initializes FetchQueue with concurrent value of 0
+  it("should not execute any fetch requests when initialized with a concurrent value of 0", () => {
+    const fetchQueue = new FetchQueue({ concurrent: 0 });
+
+    const mockFetch = jest.fn();
+
+    fetchQueue.initQueue();
+
+    fetch("https://example.com", { method: "GET" });
+
+    expect(mockFetch).not.toHaveBeenCalled();
+  });
+
+  // Executes multiple fetch requests successfully
+  it("should execute multiple fetch requests successfully", async () => {
+    const fetchQueue = new FetchQueue({ concurrent: 2 });
+
+    const mockFetch = jest.fn();
+
+    fetchQueue.initQueue();
+
+    await Promise.all([
+      fetchQueue["run"]("https://example.com", mockFetch, { method: "GET" }),
+      fetchQueue["run"](new URL("https://example.com"), mockFetch, {
+        method: "GET",
+      }),
+      fetchQueue["run"]("https://example.com", mockFetch, { method: "POST" }),
+    ]);
+
+    expect(mockFetch).toHaveBeenCalledTimes(3);
+  });
+
+  // Executes multiple fetch requests successfully
+  it("should execute multiple fetch requests successfully except one", async () => {
+    const fetchQueue = new FetchQueue({ concurrent: 2 });
+    fetchQueue.initQueue();
+
+    const urls = [
+      "https://example.com/1",
+      "https://example.com/2",
+      "https://example.com/3",
+      "https://example.com/4",
+    ];
+
+    // Mock the fetch function to simulate success for the first three calls and failure for the fourth call.
+    const mockfetch = jest.fn().mockImplementation((url) => {
+      if (url === urls[3]) {
+        return Promise.reject("Fetch failed"); // Simulate a failed fetch
+      }
+      return Promise.resolve({ status: 200, text: () => "Mocked response" });
+    });
+
+    // Execute 4 fetch calls concurrently
+    const promises = urls.map((url) => mockfetch(url));
+
+    // Wait for all promises to settle (either resolve or reject)
+    const responses = await Promise.allSettled(promises);
+
+    // Assert that 3 fetch calls succeeded
+    expect(responses.filter((r) => r.status === "fulfilled")).toHaveLength(3);
+
+    // Assert that 1 fetch call failed
+    expect(responses.find((r) => r.status === "rejected")).toBeDefined();
+  });
+});
