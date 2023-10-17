48624874084d7b213f30bf17abf7fe5a76e6e2bb
hemant-fundwave <140075591+hemant-fundwave@users.noreply.github.com>
2023-10-17 23:53:06 +0530

    feat: export cjs and esm (#2)
    * export module as both cjs and esm
    * defaults to cjs [bump minor]

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ export default {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  moduleNameMapper: { "node-fetch": "<rootDir>/node_modules/node-fetch-jest" },
+  moduleNameMapper: { "node-fetch": "<rootDir>/node_modules/node-fetch" },
 };
