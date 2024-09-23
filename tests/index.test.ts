7f4ddf97d301e3eeb0936783609487ed3fe9cfd0
hemant-fundwave <hemant@fundwave.com>
2024-09-23 15:01:34 +0530

    update: test cases with new dummy url

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 import { FetchQueue } from "../src/index";
 
-const urls = ["https://dummy.restapiexample.com/api/v1/fail", "https://dummyjson.com/products/1", "https://dummyjson.com/products/2", "https://dummyjson.com/products/3"];
+const urls = ["http://httpstat.us/500", "https://dummyjson.com/products/1", "https://dummyjson.com/products/2", "https://dummyjson.com/products/3"];
 
 async function wait(time: number = 1200) {
   return new Promise((resolve) => {
